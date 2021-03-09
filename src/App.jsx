b45083feeaa3633ccaec116093044deb273e3208
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-03-08 21:44:49 -0800

    Wip Nav, install emotion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-
+import Navigation from "./components/Navigation";
+import "./App.css";
 const App = () => {
   return (
     <>
+      <Navigation />
       <h1>Type'sy</h1>
       <p>
         A typing game that something will type when you type... How fast can you
