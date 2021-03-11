8014402c9a664b67b4eacd4a17bef54d80f38aeb
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-03-11 13:15:05 -0800

    Basic Nav and Logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react";
+import Logo from "../assets/typeLogo.png";
 
 const color = "white";
 const Navigation = () => {
   return (
-    <div>
+    <div
+      css={css`
+        max-width: 1280px;
+      `}
+    >
       <nav
         css={css`
-          padding: 32px;
+          padding: 1px;
           background-color: hotpink;
           font-size: 24px;
-          border-radius: 4px;
-          &:hover {
-            background-color: ${color};
-          }
+          height: 100px;
+          display: flex;
         `}
       >
-        <p>I will be a nav bar some day</p>
+        <img
+          src={Logo}
+          alt="Logo"
+          css={css`
+            padding: 1%;
+            width: 240px;
+          `}
+        />
       </nav>
     </div>
   );
